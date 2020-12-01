d706520230a7b1f57c7d558c7b0ab10313486bf5
Guilherme Higa <guilhermehiga11@gmail.com>
2020-12-01 16:53:24 -0300

    Incrementação da variável nome

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,5 +1,12 @@
 package Application;
 
-public class Program {
+import java.util.Scanner;
 
+public class Program {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+
+		System.out.println("Digite seu nome : ");
+		String nome = ler.nextLine();
+	}
 }
